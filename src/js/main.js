7026d792a7bf4f14e0e646f4364fd6b1fd10fdcf
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-18 10:02:36 +0200

    add border color

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const nameUser = document.querySelector(".js-name");
+const title = document.querySelector(".js-preview");
+
+nameUser.addEventListener("input", () => {
+    console.log("La usuaria está escribiendo");
+})
