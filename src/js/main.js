f8517e5d33dcdbfede573e8c0341803685b91e48
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-21 18:40:39 +0200

    add special input preview

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,12 @@ const title = document.querySelector(".js-preview");
 nameUser.addEventListener("input", (event) => {
     title.innerHTML = event.target.value;
 })
+
+
+const inputSpecial = document.querySelector(".js-special-input");
+const title2 = document.querySelector(".js-special");
+
+inputSpecial.addEventListener("input", (event) => {
+    title2.innerHTML = event.target.value;
+})
+
