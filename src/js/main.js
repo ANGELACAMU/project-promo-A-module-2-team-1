e6c4524d77ddff3c912a704ce61f2829f614c4ec
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-22 17:42:32 +0200

    add previews

--- src/js/main.js
+++ src/js/main.js
@@ -14,5 +14,37 @@ const title2 = document.querySelector(".js-special");
 inputSpecial.addEventListener("input", (event) => {
     title2.innerHTML = event.target.value;
 })
+const tank = document.querySelector(".js-tank");
+const heal = document.querySelector(".js-heal");
+const dps = document.querySelector(".js-dps");
+const title3 = document.querySelector(".js-skill");
 
+
+
+
+tank.addEventListener("input", (event) => {
+    title3.innerHTML = event.target.value;
+})
+
+heal.addEventListener("input", (event) => {
+    title3.innerHTML = event.target.value;
+})
+
+dps.addEventListener("input", (event) => {
+    title3.innerHTML = event.target.value;
+})
+
+const title4 = document.querySelector(".js-creature");
+const creature = document.querySelector(".js-list-creature");
+
+creature.addEventListener("input", (event) => {
+    title4.innerHTML = event.target.value;
+})
+
+const title5 = document.querySelector(".js-class");
+const listClass = document.querySelector(".js-list-class");
+
+listClass.addEventListener("input", (event) => {
+    title5.innerHTML = event.target.value;
+})
 
