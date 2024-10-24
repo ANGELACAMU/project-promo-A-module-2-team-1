d6d954f93a6f39585bc7f20bacec1b3904608606
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-24 14:04:23 +0200

    change main

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//NOMBRE
 const nameUser = document.querySelector(".js-name");
 const title = document.querySelector(".js-preview");
 
@@ -7,19 +7,18 @@ nameUser.addEventListener("input", (event) => {
     title.innerHTML = event.target.value;
 })
 
-
+//ESPECIAL FINISHER
 const inputSpecial = document.querySelector(".js-special-input");
 const title2 = document.querySelector(".js-special");
 
 inputSpecial.addEventListener("input", (event) => {
     title2.innerHTML = event.target.value;
 })
+//ROL
 const tank = document.querySelector(".js-tank");
 const heal = document.querySelector(".js-heal");
 const dps = document.querySelector(".js-dps");
 const title3 = document.querySelector(".js-skill");
-
-
 
 
 tank.addEventListener("input", (event) => {
@@ -33,6 +32,7 @@ heal.addEventListener("input", (event) => {
 dps.addEventListener("input", (event) => {
     title3.innerHTML = event.target.value;
 })
+//CRIATURAS
 
 const title4 = document.querySelector(".js-creature");
 const creature = document.querySelector(".js-list-creature");
@@ -41,6 +41,7 @@ creature.addEventListener("input", (event) => {
     title4.innerHTML = event.target.value;
 })
 
+//CLASES
 const title5 = document.querySelector(".js-class");
 const listClass = document.querySelector(".js-list-class");
 
