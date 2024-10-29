92c8378f1eb6d1fd19854f3d373e09dd20b6414d
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-29 13:27:17 +0100

    rotate button

--- src/js/collapsed.js
+++ src/js/collapsed.js
@@ -6,12 +6,16 @@ const jsbnsk = document.querySelector(".js-btn-add-skill")
 const Desplega = document.querySelector(".js-collapsed");
 const Desplegaskill = document.querySelector(".js-collapsed-skill");
 const Desplegash = document.querySelector(".js-collapsed-sh");
-const jsbnsh = document.querySelector(".js-btn-add-sh")
+const jsbnsh = document.querySelector(".js-btn-add-sh");
 
+
+jsbtnadd.classList.toggle('js-btn-rotate');
 
 function CollFormu() {
   Desplega.classList.toggle('collapsed');
+  jsbtnadd.classList.toggle('js-btn-rotate');
   console.log("apretar boton");
+
 };
 
 jsbtnadd.addEventListener("click", CollFormu);
@@ -21,6 +25,7 @@ Desplegaskill.classList.add('collapsed');
 
 function CollFormuSkill() {
   Desplegaskill.classList.toggle('collapsed');
+  jsbnsk.classList.toggle('js-btn-rotate');
   console.log("apretar boton");
 
 };
@@ -29,6 +34,7 @@ jsbnsk.addEventListener("click", CollFormuSkill);
 Desplegash.classList.add('collapsed');
 function CollFormuSh() {
   Desplegash.classList.toggle('collapsed');
+  jsbnsh.classList.toggle('js-btn-rotate');
   console.log("apretar boton");
 };
 jsbnsh.addEventListener("click", CollFormuSh);
